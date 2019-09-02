231d5b4c223f44c9e4f528c72458b719c7df6c97
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-02 13:22:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,14 @@
 def monopoly
   monopoly = {
-    railroads: {}
+    railroads: {
+      pieces: 4
+    }
   }
 end
 
 def monopoly_with_second_tier
   monopoly = {
     railroads: {
-      pieces: 4,
       names: {},
       rent_in_dollars: {}
     }
